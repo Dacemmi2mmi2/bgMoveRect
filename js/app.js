5f2803d29e7a63a889f2aa7da464f0174d312fe5
Dacemmi2mmi2 <dshevchenko507@gmail.com>
2021-02-21 09:55:59 +0200

    changed qantity blocks rects for mob screen

--- js/app.js
+++ js/app.js
@@ -1,28 +1,63 @@
 const htmlElements = {
     blockAnimation : document.querySelector('.animationRect'),
+    textBlockAnimation : document.querySelector('.animationRect p'),
 }
 
 
-for (let i = 0; i <= 99; i++)
+const moveRect = function moveRectangles(height, width)
 {
-    let blockRect = document.createElement('div');
-    blockRect.classList.add('blockRect');
-    htmlElements.blockAnimation.appendChild(blockRect);
-    blockRect.style.left = Math.floor(Math.random() * (100 - (-10))) + (-10) + '%';
-    blockRect.style.top = Math.floor(Math.random() * (100 - (-10))) + (-10) + '%';
-    blockRect.style.transform = `scale(${Math.floor(Math.random() * (5 -2)) + 2})`;
+    let quantityRects,
+        scaleMax,
+        scaleMin;
+
+    if (width > 300){
+        quantityRects = 24;
+        scaleMax = 6;
+        scaleMin = 3;
+    }
+    if (width > 600){
+        quantityRects = 99;
+        scaleMax = 4;
+        scaleMin = 2;
+    }    
+console.log(quantityRects);
+    while (htmlElements.textBlockAnimation.nextElementSibling) {
+        htmlElements.blockAnimation.removeChild(htmlElements.textBlockAnimation.nextElementSibling);
+    }
+
+    for (let i = 0; i <= quantityRects; i++)
+    {
+        let blockRect = document.createElement('div');
+        blockRect.classList.add('blockRect');
+        htmlElements.blockAnimation.appendChild(blockRect);
+        blockRect.style.left = Math.floor(Math.random() * (100 - (-10))) + (-10) + '%';
+        blockRect.style.top = Math.floor(Math.random() * (100 - (-10))) + (-10) + '%';
+        blockRect.style.transform = `scale(${Math.floor(Math.random() * (scaleMax - scaleMin)) + scaleMin})`;
+    }
+
+
+    let posRect = function positionBlockRect()
+    {
+        setInterval(() => {
+            let allBlocksRect = document.querySelectorAll('.blockRect');
+                allBlocksRect.forEach(item => {
+                item.style.left = Math.floor(Math.random() * (100 - (-10))) + (-10) + '%';
+                item.style.top = Math.floor(Math.random() * (100 - (-10))) + (-10) + '%';
+                item.style.transform = `scale(${Math.floor(Math.random() * (scaleMax - scaleMin)) + scaleMin})`;
+            });
+        }, 4000);
+    }
+    posRect();
 }
 
 
-const posRect = function positionBlockRect()
+const paramScreen = function parametersScreen()
 {
-    setInterval(() => {
-        let allBlocksRect = document.querySelectorAll('.blockRect');
-        allBlocksRect.forEach(item => {
-            item.style.left = Math.floor(Math.random() * (100 - (-10))) + (-10) + '%';
-            item.style.top = Math.floor(Math.random() * (100 - (-10))) + (-10) + '%';
-            item.style.transform = `scale(${Math.floor(Math.random() * (4 -2)) + 2})`;
-        });
-    }, 4000);
+    moveRect(innerHeight, innerWidth);
 }
-posRect();
+paramScreen();
+
+
+window.addEventListener('resize', () => {
+    paramScreen();
+});
